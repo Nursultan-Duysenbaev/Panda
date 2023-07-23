92d0b8e3f74df2fda34219f9d31eae8bf865d528
Nursultan- Duysenbaev <2110013@newuzbekistanuniversity.uz>
2023-07-23 18:43:58 +0500

    Initial Commit

---
+++ Panda/PandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaApp.swift
+//  Panda
+//
+//  Created by Nursultan Duysenbaev on 23/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
